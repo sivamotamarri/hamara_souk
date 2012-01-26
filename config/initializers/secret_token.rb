fa2301034c2ad9e314492d824b56306d12fdf397
sivakrishna <sivakrishna.motamarri@gmail.com>
2012-01-26 19:28:40 +0530

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HamaraSouk::Application.config.secret_token = '7c3f8ffed47b4505f02c346f1c174c2794efe0546351ed2cc96f0f58ccde4aa7c4daba1f98938d817e0617ba856689d90557dea22248edea4547b9704e804ec6'
