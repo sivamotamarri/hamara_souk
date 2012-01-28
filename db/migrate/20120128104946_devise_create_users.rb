9e0adfb3057df76905b84946b6b5c0e49fdf29b9
sivakrishna <sivakrishna.motamarri@gmail.com>
2012-01-28 18:51:35 +0530

    devise files added

---
+++ db/migrate/20120128104946_devise_create_users.rb
@@ -0,0 +1,36 @@
+class DeviseCreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table(:users) do |t|
+      t.database_authenticatable :null => false
+      t.recoverable
+      t.rememberable
+      t.trackable
+
+      t.encryptable
+      t.confirmable
+      t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
+      t.token_authenticatable
+
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :gender
+      t.string :country
+      t.string :professional
+      t.date :dob
+      t.boolean  :hamara_souk_updates , :default => false
+      t.boolean :allow_adv , :default => false
+      t.timestamps
+    end
+
+    add_index :users, :email,                :unique => true
+    add_index :users, :reset_password_token, :unique => true
+    add_index :users, :confirmation_token,   :unique => true
+    add_index :users, :unlock_token,         :unique => true
+    add_index :users, :authentication_token, :unique => true
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
