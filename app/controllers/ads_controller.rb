41952776ef6a09d83b959897b31b8326a1c42454
sivakrishna <sivakrishna.motamarri@gmail.com>
2012-02-13 23:05:11 +0530

    ad new page'

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -2,10 +2,20 @@ class AdsController < ApplicationController
   
   def new
     @ad = Ad.new
+    @amenities = Amenity.all
+  end
+  def step2
+    @amenities = Amenity.all
+  end
+  def step3
+     
+  end
+  def step4
+    
   end
   
   def create
-    
+    render :text => params.inspect
   end
   def show
     render :text => "heell"
