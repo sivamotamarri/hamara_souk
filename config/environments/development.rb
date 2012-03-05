a8ba711a774b5c970d7f6875b483f5fc8da033de
sivakrishna <sivakrishna.motamarri@gmail.com>
2012-03-05 22:02:10 +0530

    email settings added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,22 @@ HamaraSouk::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'hamarasouk.com' }
+  #config.action_mailer.delivery_method = :letter_opener
+  
+  config.action_mailer.raise_delivery_errors = true
+
+# set delivery method to :smtp, :sendmail or :test
+config.action_mailer.delivery_method = :smtp
+#
+## these options are only needed if you choose smtp delivery
+#config.action_mailer.smtp_settings = {
+#  :address        => 'smtp.hamarasouk.com',
+#  :port           => 25,
+#  :domain         => 'www.hamarasouk.com',
+#  :authentication => :login,
+#  :user_name      => 'support',
+#  :password       => 'Hamara123'
+#}
 end
