bf5f891847f29816c840df6463b6bdca5505b876
sivakrishna <sivakrishna.motamarri@gmail.com>
2012-04-12 23:11:56 +0530

    added tabs

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,6 +1,6 @@
 class AdsController < ApplicationController
  add_breadcrumb "Home", :root_path
- before_filter :authenticate_user!
+ before_filter :authenticate_user! ,:except => [:show]
   
   def index
     section = Section.find_by_slug(params[:section])
